2f56dcb7b8b1b1eedb19ab7f33ecd3f2113c4bd4
Jash Patel <41770078+JashUpatel@users.noreply.github.com>
2020-09-01 19:49:44 +0530

    app.use cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors  = require('cors');
 
 const app = express();
 
@@ -19,6 +20,9 @@ const mongoose = require('mongoose');
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 // to parse incoming json data in req.body
+
+// using cors
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended:false}));
 // to parse req.body coming from form data
